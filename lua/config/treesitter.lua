13d82d51434ba60fcfcd4c2cf531f87095595fdf
Kevin Pounot <kpounot@hotmail.fr>
2024-03-28 22:16:29 +0100

    bug correction with treesitter, using clang or gcc as compilers (gcc installed with scoop works fine in Windows 11 - using Windows SDK)

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -12,7 +12,7 @@ require('nvim-treesitter.configs').setup {
   sync_install = false,
   auto_install = true,
   ignore_install = { "javascript" },
-  compilers = { "clang" },
+  compilers = { "clang", "gcc" },
   highlight = {
     enable = true,
     disable = function(lang, buf)
